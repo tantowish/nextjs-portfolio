d3d38a009ccc67926462eb7824e20396c1a7aca6
tanto <tantows001@gmail.com>
2024-01-14 15:45:10 +0700

    feat: navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="w-screen h-screen relative">
       <div className="flex items-center w-full h-full bg-cover bg-center" style={{ backgroundImage: "url(/main-bg.webp)" }}>
-        <div className="pl-20 md:pl-40 pb-56 md:pb:20 flex flex-col gap-5 z-[10] max-w-[750px]">
+        <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">Make anything possbile with
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> Web Development</span>
           </h1>
